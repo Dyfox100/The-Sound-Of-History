0495483add4a125cafb38ca0c40b0f4324487203
Andrea DeVore <andreadevo@rgnt2-104-33-dhcp.int.colorado.edu>
2018-03-18 13:18:18 -0600

    moved title into a class

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import WebFont from 'webfontloader';
 import './index.css';
 
+class Main extends React.Component {
+   render(){
+      return (
+         <h1>The Sound of History</h1>
+      );
+   }
+}
+
 WebFont.load({
-  google: {
-    families: ['Playfair Display', 'serif']
+   google: {
+      families: ['Playfair Display', 'serif']
   }
 });
 
 ReactDOM.render(
-  <h1>The Sound of History</h1>,
-  document.getElementById('root')
+   <Main />,
+   document.getElementById('root')
 );
