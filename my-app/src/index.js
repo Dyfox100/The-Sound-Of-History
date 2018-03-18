e9937717cecea4c7639ad66f5a911b39e2ac17e6
marcoortiztorres <marcoortiztorres@rgntgw2-69-176-dhcp.int.colorado.edu>
2018-03-18 15:01:16 -0600

    added reactstrap to allow for buttons

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WebFont from 'webfontloader';
+// npm install --save reactstrap@next react react-dom
+import { Button } from 'reactstrap';
 import './index.css';
 
 class Main extends React.Component {
-   render(){
-      return (
-         <h1>The Sound of History</h1>
-      );
-   }
+  render(){
+    return (
+      <div>
+        <h1>The Sound of History</h1>
+        <Button color="primary" size="lg"> Search </Button>
+      </div>
+    );
+  }
 }
 
-class Button extends React.Component {
+class Search extends React.Component {
   render(){
      return (
         <h1>Button Goes Here</h1>
