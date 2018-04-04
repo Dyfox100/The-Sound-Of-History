08055b0e4d877c9fcf2792e4050c210a146efee9
Andrea DeVore <andreadevo@rgnt2-105-91-dhcp.int.colorado.edu>
2018-04-04 16:39:20 -0600

    skeleton of classes decorator and eventSuggest

---
+++ my-app/src/decorator.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { InputGroup, InputGroupAddon, Button, Input, NavLink } from 'reactstrap';
+
+class Title extends React.Component {
+   var titleName = "";
+
+   render(){
+      return(
+
+      );
+   }
+
+   setTitleName(){
+
+   }
+}
+
+class SearchDataBase extends React.Component {
+   var queryInput = ""
+
+   render(){
+      return (
+
+      );
+   }
+
+   query(){
+
+   }
+   sendRequest(){
+
+   }
+}
+
+class Button extends React.Component {
+   var buttonName = "";
+   var updateButton;
+
+   render(){
+      return (
+
+      );
+   }
+   setButtonName(){
+
+   }
+   setUpdateButton(){
+
+   }
+   getUpdateButton(){
+
+   }
+}
+
+class Footer extends React.Component {
+   var stateName = "";
+
+   render(){
+      return (
+
+      );
+   }
+   redirect(){
+
+   }
+   getStateName(){
+
+   }
+   setStateName(){
+
+   }
+}
