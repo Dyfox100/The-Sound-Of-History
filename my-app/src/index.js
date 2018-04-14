ab589da3601c7fe713c0d27ac065ad216786d4e7
marcoortiztorres <maor9361@colorado.edu>
2018-04-14 15:47:09 -0600

    Search Page class is working properly. Currently all it does it put your searched item into an alert. But it is ready to be connected with the SQL query.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { InputGroup, InputGroupAddon, Button, Input, NavLink } from 'reactstrap';
 
 import logo from './img/soundOfMusic.png'
+import {SearchPage, SearchBar, DescriptionBox} from "./components/searchPage";
 import './index.css';
 
 class Home extends React.Component {
@@ -14,54 +15,10 @@ class Home extends React.Component {
       <div>
         <img src={logo} class="title"/>
         <br/>
-        <Search/>
-        <br/>
-        <Description/>
+        <SearchPage/>
       </div>
     );
   }
-}
-
-class Search extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      text: 'Temp Value'
-    };
-  }
-
-  updateSearch(){
-    this.setState( { text: this.refs.searchField.value } );
-    console.log("loading");
-  }
-
-  render(){
-    return (
-      <div id="searchBox">
-        <InputGroup>
-          <Input placeholder="Enter a date, song, or event" ref="searchField" type="text"/>
-          <InputGroupAddon addonType="append">
-            <Button color="secondary"
-              onClick={ (e) => { this.updateSearch(); } }>
-              <NavLink> Search </NavLink>
-            </Button>
-          </InputGroupAddon>
-        </InputGroup>
-        <br/>
-        {this.state.text}
-      </div>
-    );
-  }
-}
-
-class Description extends React.Component {
-   render(){
-      return (
-         <div>
-            <p>Description of Project goes here.</p>
-         </div>
-      );
-   }
 }
 
 WebFont.load({
