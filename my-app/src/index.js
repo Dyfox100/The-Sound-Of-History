4ecb626de4661d53650a091a29d9ab173a454aba
Andrea DeVore <andreadevo@rgnt2-107-9-dhcp.int.colorado.edu>
2018-04-15 16:31:29 -0600

    worked on parsing song jsob object in resultsPage. modified index.js for this branch so results page will render

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,17 +5,14 @@ import WebFont from 'webfontloader';
 import 'bootstrap/dist/css/bootstrap.css';
 import { InputGroup, InputGroupAddon, Button, Input, NavLink } from 'reactstrap';
 
-import logo from './img/soundOfMusic.png'
-import {SearchPage, SearchBar, DescriptionBox} from "./components/searchPage";
+import {ResultsPage, HistoryBox, SongBox} from "./resultsPage.js"
 import './index.css';
 
 class Home extends React.Component {
   render(){
     return (
       <div>
-        <img src={logo} class="title"/>
-        <br/>
-        <SearchPage/>
+        <ResultsPage/>
       </div>
     );
   }
--- my-app/src/resultsPage.js
+++ my-app/src/resultsPage.js
@@ -8,12 +8,26 @@ import { InputGroup, InputGroupAddon, Button, Input, NavLink } from 'reactstrap'
 // import './index.css';
 
 export class ResultsPage extends React.Component {
-    //var queriedItems = "";
+    queriedItems: "";
     render(){
-        const history = ['1', '2','3','4','5','6','7','8','9','10']
+        const history = ['1', '2','3','4','5','6','7','8','9','10'];
+        const songs = {
+           song1: "This is a test song",
+           artist1: "This is an artist",
+           peakPosition1: "This is a peak position",
+           lastWeek1: "This is last week",
+           weeksOnChart1: "This is weeks",
+           song2: "This is a test song 2",
+           artist2: "This is an artist 2",
+           peakPosition2: "This is a peak position 2",
+           lastWeek2: "This is last week 2",
+           weeksOnChart2: "This is weeks 2"
+        };
         return (
             <div>
                 <HistoryBox history = {history}/>
+                <br/>
+                <SongBox songs = {songs}/>
             </div>
         );
     }
@@ -22,7 +36,7 @@ export class ResultsPage extends React.Component {
     }
 }
 
-// class DateBox extends React.Component {
+// export class DateBox extends React.Component {
 //     var date = "";
 
 //     render(){
@@ -38,23 +52,55 @@ export class ResultsPage extends React.Component {
 //     }
 // }
 
-// class SongBox extends React.Component {
-//     var songList = "";
+export class SongBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.songs = this.props.songs;
+        this.parseSongs = this.parseSongs.bind(this);
+    }
+    parseSongs(){
+        var song = "";
+        var artist = "";
+        var peak = "";
+        var last = "";
+        var weeks = "";
+        let songList = []
 
-//     render(){
-//         return (
+        // store songs/artists/info in a list of objects
+        for (var i=1; i<3; i++){
+            song = "song" + i.toString();
+            artist = "artist" + i.toString();
+            peak = "peakPosition" + i.toString();
+            last = "lastWeek" + i.toString();
+            weeks = this.songs["weeksOnChart" + i.toString()];
+            // var rank = {
+            //
+            // }
+            // songList[i-1] = rank;
+        }
+    }
+    render(){
+        return (
+            <h1>TEST</h1>
+            // <div>
+            //     <ol>{
+            //         this.songs.slice(0,10).map((item) => {
+            //             return<li> {item}</li>
+            //         })
+            //     }
+            //     </ol>
+            // </div>
+        );
+    }
+    setSongList(){
 
-//         );
-//     }
-//     setSongList(){
+    }
+    getSongList(){
 
-//     }
-//     getSongList(){
+    }
+}
 
-//     }
-// }
-
-class HistoryBox extends React.Component {
+export class HistoryBox extends React.Component {
     constructor(props){
         super(props);
         this.history = this.props.history;
@@ -90,4 +136,3 @@ WebFont.load({
         families: ['Playfair Display', 'serif']
     }
 });
-
