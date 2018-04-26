b157be7aa1474eacfbdaa7b3092c6a138437941a
Andrea DeVore <ande2623@colorado.edu>
2018-04-25 19:50:04 -0600

    populated event suggest page using a jsob file in a new attributes folder

--- my-app-head-directory/my-app/src/components/eventSuggestPage.js
+++ my-app-head-directory/my-app/src/components/eventSuggestPage.js
@@ -1,9 +1,13 @@
 // ----- END IMPORTS -----
 import React from 'react';
 import ReactDOM from 'react-dom';
+//ALL
 
 import WebFont from 'webfontloader';
 import { InputGroup, InputGroupAddon, Button, Input, NavLink } from 'reactstrap';
+
+// Import json Event Suggest file with hardCode 
+import EventQuery from '../attributes/EventQuery.json';
 
 // import css files
 import '../index.css';
@@ -12,13 +16,99 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 // ----- INTERFACE -----
 export class EventSuggest extends React.Component {
+	constructor(props){
+		super(props)
+		this.EventQuery = EventQuery;
+	}
     render(){
+    	// console.log(EventQuery["2010s"][0].Event);
+    	// let eventLength = console.log((this.EventQuery["2010s"]).length);
         return (
-            <h1>Event Suggestions</h1>
+        	<div>
+        	<h1>Event Suggest Page</h1>
+        		<h2>1960</h2>
+        			<div>
+        				<ul>
+        					{this.EventQuery["1960s"].map((eventObject) => {
+        						return (
+        							<li>
+        								{eventObject.Year}: {eventObject.Event}
+          							</li>
+        						)
+        					})
+        					}
+        				</ul>
+        			</div>
+        		<h2>1970</h2>
+        			<div>
+        				<ul>
+        					{this.EventQuery["1970s"].map((eventObject) => {
+        						return (
+        							<li>
+        								{eventObject.Year}: {eventObject.Event}
+          							</li>
+        						)
+        					})
+        					}
+        				</ul>
+        			</div>
+        		<h2>1980</h2>
+        			<div>
+        				<ul>
+        					{this.EventQuery["1980s"].map((eventObject) => {
+        						return (
+        							<li>
+        								{eventObject.Year}: {eventObject.Event}
+          							</li>
+        						)
+        					})
+        					}
+        				</ul>
+        			</div>
+        		<h2>1990</h2>
+        			<div>
+        				<ul>
+        					{this.EventQuery["1990s"].map((eventObject) => {
+        						return (
+        							<li>
+        								{eventObject.Year}: {eventObject.Event}
+          							</li>
+        						)
+        					})
+        					}
+        				</ul>
+        			</div>
+        		<h2>2000</h2>
+        			<div>
+        				<ul>
+        					{this.EventQuery["2000s"].map((eventObject) => {
+        						return (
+        							<li>
+        								{eventObject.Year}: {eventObject.Event}
+          							</li>
+        						)
+        					})
+        					}
+        				</ul>
+        			</div>
+        		<h2>2010</h2>
+        			<div>
+        				<ul>
+        					{this.EventQuery["2010s"].map((eventObject) => {
+        						return (
+        							<li>
+        								{eventObject.Year}: {eventObject.Event}
+          							</li>
+        						)
+        					})
+        					}
+        				</ul>
+        			</div>
+        	</div>
+
+        		
         );
     }
-   //  listYearBoxes(){
-   // }
 }
 // ----- END OF INTERFACE -----
 
