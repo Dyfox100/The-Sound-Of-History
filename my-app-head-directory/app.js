4729d2bcaef6f6455af45acb87f835e383bac88e
Dyfox100 <Dylan.Fox@colorado.edu>
2018-04-28 20:44:58 -0600

    changing app.js mongo connection back to localhost to test

--- my-app-head-directory/app.js
+++ my-app-head-directory/app.js
@@ -11,7 +11,7 @@ var client = new elasticsearch.Client({
 var mongoClient = mongodb.MongoClient;
 var mclient;
 
-mongoClient.connect('mongodb://52.37.120.142:27017', (err, client1) => {
+mongoClient.connect('mongodb://localhost:27017', (err, client1) => {
     if (err) {
         throw err;
     } else {
