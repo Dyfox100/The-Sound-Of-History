c60f710dade474f735d70f341fd0fb02e7bfa885
ACodeNameGui <32375861+b-sinnett25@users.noreply.github.com>
2018-04-30 15:16:36 -0600

    commiting to elastic search

--- ElasticSearch/init_replica.js
+++ ElasticSearch/init_replica.js
@@ -7,5 +7,11 @@ config = { _id: "bbtop100", members:[
 			{_id : 2, host : "localhost:27019"}]
 };
 
+config = { _id: "bbtop100", members:[
+			{_id : 0, host : "localhost:27017"},
+			{_id : 1, host : "localhost:27018"}]
+};
+
+
 rs.initiate(config);
 rs.status();
